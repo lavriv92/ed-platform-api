833fdf6ebf3181a9bb1c44590ea829f99421500e
Ivan Lavriv <Ivan.Lavriv@irco.com>
2017-02-09 21:55:49 +0200

    Added paths and users route to admin

--- apps/admin/config/routes.rb
+++ apps/admin/config/routes.rb
@@ -3,4 +3,5 @@
 #
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
-get '/', to: 'admin_home_controller#index'
+get '/users', to: 'users_controller#index', as: :users
+root to: 'admin_home_controller#index'
---
+++ apps/admin/views/users_controller/index.rb
@@ -0,0 +1,5 @@
+module Admin::Views::UsersController
+  class Index
+    include Admin::View
+  end
+end
